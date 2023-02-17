9294ed7a2817125c41ad0aab9a2f82c478254836
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-17 17:07:13 +0000

    Imported datatypes and sequelize in from the sequelize library, established a new db connection and exported the datatypes method and sequlize connection

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
 const path = require('path');
-const { Sequelize } = require('sequelize');
+const { DataTypes, Sequelize } = require('sequelize');
 
 // TODO - connect to db via sequelize
 const sequelize = new Sequelize({
     dialect: "sqlite",
+    // Datatypes: ,  
     storage: path.join(__dirname, "db.sqlite")
 })
 
 module.exports = {
-    sequelize
+    DataTypes,
+    sequelize,
+    // Sequelize
 };
