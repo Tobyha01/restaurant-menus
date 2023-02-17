aab7a1b4524f26dad71e4d3b9a849033f0cc4684
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-17 17:15:15 +0000

    Created a menu model and defined it's property names and datatypes

--- models/Menu.js
+++ models/Menu.js
@@ -1,6 +1,9 @@
-const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const {DataTypes, sequelize} = require('../db');
+// const { Sequelize } = require('sequelize');
 
 // TODO - create a Menu model
+const Menu = sequelize.define({
+    title: DataTypes.STRING
+})
 
 module.exports = {Menu};
