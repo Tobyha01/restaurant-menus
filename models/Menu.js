832c4dada0ec134ea5a339adb0176fc7de01248e
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-18 14:33:26 +0000

    Fixed a syntax issue in regards to creating a new model for the restaurant and menu, in the respected files

--- models/Menu.js
+++ models/Menu.js
@@ -2,7 +2,7 @@ const {DataTypes, sequelize} = require('../db');
 // const { Sequelize } = require('sequelize');
 
 // TODO - create a Menu model
-const Menu = sequelize.define({
+const Menu = sequelize.define("menu", {
     title: DataTypes.STRING
 })
 
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -2,7 +2,7 @@ const {sequelize, DataTypes, /* Sequelize */} = require('../db');
 // const {  } = require('sequelize');
 
 // TODO - create a Restaurant model
-const Restaurant = sequelize.define ({
+const Restaurant = sequelize.define ("restaurant", {
     name: DataTypes.STRING,
     location: DataTypes.STRING,
     cuisine: DataTypes.STRING
