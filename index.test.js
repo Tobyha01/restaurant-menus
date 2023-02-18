7c3a575b4c2a17b7f5bd9190b8f3951fb3637418
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-18 14:31:12 +0000

    Added a test to check the reastaurant database could successfully create a new entry

--- index.test.js
+++ index.test.js
@@ -1,43 +1,46 @@
-const {sequelize} = require('./db')
+const {sequelize, DataTypes} = require('./db')
 const {Restaurant, Menu} = require('./models/index')
 const {
     seedRestaurant,
     seedMenu,
   } = require('./seedData');
 
-describe('Restaurant and Menu Models', () => {
+describe('Restaurant and Menu Models', function() {
     /**
      * Runs the code prior to all tests
      */
-    beforeAll(async () => {
+    beforeAll(async function() {
         // the 'sync' method will create tables based on the model class
         // by setting 'force:true' the tables are recreated each time the 
         // test suite is run
         await sequelize.sync({ force: true });
     });
 
-    test('can create a Restaurant', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+        test('can create a Restaurant', async function() {
+            // const restaurant1 = seedRestaurant[0].name
+            await Restaurant.create(seedRestaurant[0])
+            expect(seedRestaurant[0].name).toEqual('AppleBees')
+            expect(seedRestaurant[0].location).toEqual('Texas')
+            expect(seedRestaurant[0].cuisine).toEqual('FastFood')
+        });
 
-    test('can create a Menu', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+    //     test('can create a Menu', async function() {
+    //         // TODO - write test
+    //         expect('NO TEST').toEqual('EXPECTED DATA')
+    //     });
 
-    test('can find Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+    //     test('can find Restaurants', async function() {
+    //         // TODO - write test
+    //         expect('NO TEST').toEqual('EXPECTED DATA')
+    //     });
 
-    test('can find Menus', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+    //     test('can find Menus', async function() {
+    //         // TODO - write test
+    //         expect('NO TEST').toEqual('EXPECTED DATA')
+    //     });
 
-    test('can delete Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
-})
+    //     test('can delete Restaurants', async function() {
+    //         // TODO - write test
+    //         expect('NO TEST').toEqual('EXPECTED DATA')
+    //     });
+    })
